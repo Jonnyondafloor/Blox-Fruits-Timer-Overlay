395af143b47d783183c6eca7f2d73366ade7fca0
Jonnyondafloor <jonathan6474231560715@gmail.com>
2025-05-07 06:20:25 +1200

    Created Timer Template/Class for refactor

---
+++ Timers/template.py
@@ -0,0 +1,24 @@
+import tkinter as tk
+from PIL import Image, ImageTk
+from typing import Callable
+
+tk.Tk()
+
+class Timer:
+    def __init__(self, image_path: str):
+        self.image_path = image_path
+        self.image = self._load_image()
+        self.title = ''
+        self.description = ''
+        self.auto_reset_enabled = True
+        self.auto_reset_delay = 0
+        self.delay_method: Callable[[], float] = None
+        self.reset_method: Callable[[], float] = None
+
+    def _load_image(self) -> ImageTk.PhotoImage | None:
+        try:
+            img = Image.open(self.image_path).resize((50, 60))
+            return ImageTk.PhotoImage(img)
+        except FileNotFoundError:
+            print(f'Error: Image file not found at {self.image_path}')
+            return None
